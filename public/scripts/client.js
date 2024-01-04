20a200077e4caba6bd646497eb877f9d7b85dd04
Racingwind <louis.k.hu@gmail.com>
2024-01-04 00:53:34 -0800

    created a load tweets client side js function to send AJAX GET calls to http://localhost:8080/tweets to fetch tweets data and render them

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  // sample data to work with
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
-
   // function to create a tweet article element
   const createTweetElement = (data) => {
     const difference = Date.now() - data.created_at;
@@ -64,7 +37,7 @@ $(document).ready(function() {
     });
   };
 
-  renderTweets(data);
+  // renderTweets(data);
 
 
   // Listen for form submit and prevent default actions
@@ -79,4 +52,16 @@ $(document).ready(function() {
       data: $formData
     });
   });
+  
+
+  // function to load tweets from server
+  const loadTweets = () => {
+    $.ajax('http://localhost:8080/tweets', { method: 'GET' })
+    .then(function (data) {
+      console.log(data);
+      renderTweets(data);
+    });
+  };
+
+  loadTweets();
 });
